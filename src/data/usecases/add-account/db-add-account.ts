dbcd5965c1b77586c83aef1933148ba5acd842c7
Eduardo Maciel <mrgamex49@gmail.com>
2021-04-18 22:48:31 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,23 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './db-add-account-protocols'
+import { AccountModel, AddAccount, AddAccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
-  constructor (encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository
+
+  constructor (encrypter: Encrypter, addAccount: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccount
   }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
-    const accountData = {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign(accountData, { password: hashedPassword }))
+    const account = { // bypass typescript interface
       id: 'valid_id',
-      email: account.email,
-      password: account.password,
-      name: account.name
+      email: accountData.email,
+      password: accountData.password,
+      name: accountData.name
     }
-    return await new Promise(resolve => resolve(accountData))
+    return await new Promise(resolve => resolve(account))
   }
 }
