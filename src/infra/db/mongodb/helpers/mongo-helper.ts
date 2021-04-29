514fa95ffdd0b1f39ff78b1f5e0d2a530fa71c50
Eduardo Maciel <mrgamex49@gmail.com>
2021-04-28 23:58:12 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,13 +1,19 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
+
 export const MongoHelper = {
-  client: MongoClient,
+  // @ts-expect-error
+  client: null as MongoClient,
+
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect('', {
+    this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
   },
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
